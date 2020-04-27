76a6887c1fef86298ef4718454631aa9f1d3bbf9
fedir72 <fedir72@ua.fm>
2020-04-27 19:57:07 +0300

    first example interval version

---
+++ networkingExamples/networkingExamples/ Models/Post.swift
@@ -0,0 +1,16 @@
+//
+//  Post.swift
+//  networkingExamples
+//
+//  Created by fedir on 27.04.2020.
+//  Copyright © 2020 fedir. All rights reserved.
+//
+
+import Foundation
+
+class Post: Codable {
+    var userId: Int
+    var id: Int
+    var title: String
+    var body: String
+}
--- networkingExamples/networkingExamples/Controllers/FirstViewController.swift
+++ networkingExamples/networkingExamples/Controllers/FirstViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var textlabel: UILabel!
+    
+    var dataTask: URLSessionDataTask?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +20,37 @@ class FirstViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func DownloadPostbutton(_ sender: UIButton) {
+        
+        let stringURL = "https://jsonplaceholder.typicode.com/posts"
+        if let url = URL(string: stringURL) {
+            dataTask = URLSession.shared.dataTask(with: url) { (data, responce, error) in
+                
+                if error != nil {
+                    print("error in request")
+                }else{
+                    if let resp = responce as? HTTPURLResponse , resp.statusCode == 200 ,let responcedata = data {
+                        //print(resp.statusCode)
+                        print(Thread.current)
+                       //print(resp)
+                        //print(responcedata)//обьем загруженных данных
+                        let posts = try? JSONDecoder().decode([Post].self, from: responcedata)
+                         print(posts)
+                        DispatchQueue.main.async {
+                            self.textlabel.text =  "post has been downloaded"
+                       }
+                        
+                       
+                    }
+                }
+            }
+            dataTask?.resume()
+            
+            
+            
+            
+        }
+        
     }
-    */
-
+    
 }
