f550579e6f0be71a2108c25a19c386e79df41bd8
fedir72 <fedir72@ua.fm>
2020-04-30 17:50:10 +0300

    a 2nd example end (weatherstack)

--- networkingExamples/networkingExamples/Controllers/SecondViewController.swift
+++ networkingExamples/networkingExamples/Controllers/SecondViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import MapKit
 
 class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var tempCelsiuslabel: UILabel!
+    @IBOutlet weak var localTimeLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    
+    @IBOutlet weak var weaterIconLabel: UIImageView!
+    
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         searchBar.delegate = self
+        mapView.delegate = self
+        
     }
     
 
@@ -23,7 +38,72 @@ class SecondViewController: UIViewController {
 
 extension SecondViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        print(searchBar.text!)
+        
+        print("SearchbarText: ",searchBar.text!)
+        
+        var locationName: String?
+        var temperatureCelsius: Int?
+        var countryname: String?
+        var timeLocal: String?
+        var errorHasOccured = false
+        //var imageWeather: String?
+        
+        
+        let strUrl = "http://api.weatherstack.com/current?access_key=bce1e689d3e7499371dd62da5b22794a&query=\(searchBar.text! .replacingOccurrences(of: " ", with:"%20"))"
+        let url = URL(string: strUrl)
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            do {
+            let json = try  JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String:AnyObject]
+               print(json)
+                
+                //MARK: - срабатывает при ошибке
+                if let _ = json["error"] {
+                    errorHasOccured = true
+                }
+                
+                if let location = json["location"] {
+                 
+                    locationName = location["name"] as? String
+                    countryname = location["country"] as? String
+                    timeLocal = location["localtime"] as? String
+                    
+                    //print(locationName)
+                }
+                
+                if let current = json["current"] {
+                    temperatureCelsius = current["temperature"] as? Int
+                }
+                
+                DispatchQueue.main.async {
+                    
+                    if errorHasOccured {
+                        //MARK: - сработает если значениe не найдено
+                        self.nameLabel.text = "Локация не найдена"
+                    } else {
+                self.nameLabel.text = "City: " + (locationName!)
+                self.tempCelsiuslabel.text = "Temperature ℃: \(temperatureCelsius!)"
+                self.countryLabel.text = countryname!
+                self.localTimeLabel.text = "LocalTime: " + (timeLocal! )
+                self.searchBar.text = ""
+                    }
+                    
+                }
+            }catch{
+                print(error)
+            }
+            
+            
+        }
+        
+        task.resume()
     }
+    
 }
 
+
+extension SecondViewController: MKMapViewDelegate {
+    
+    
+    
+    }
